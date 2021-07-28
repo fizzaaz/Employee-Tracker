3bf328e6b68340ceb657c4ffbcc4f0e0060e9245
unknown <fizz.zehra14@gmail.com>
2021-07-28 07:09:42 -0500

    SCHEMA CREATED

--- db/schema.sql
+++ db/schema.sql
@@ -9,15 +9,15 @@ USE EmployeeDB;
 -- CREATING DEPARTMENT TABLE ----
 CREATE TABLE department(
     id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(30)
+    name VARCHAR(30)  NOT NULL
 )
 
 -- CREATING ROLE TABLE ----
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    title  VARCHAR(30),
-    salary DECIMAL(10,2),
-    department_id INTEGER,
+    title  VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INTEGER NOT NULL,
     --INT to hold reference to department role belongs to-
     FOREIGN KEY department_id REFERENCES department(id)
 )
@@ -25,13 +25,14 @@ CREATE TABLE role (
 -- CREATING EMPLOYEE TABLE ----
 CREATE TABLE employee (
     id INTEGER AUTO AUTO_INCREMENT PRIMARY KEY  NOT NULL,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INTEGER,
-    --INT to hold reference to another employee that is manager of the current employee--
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
     manager_id INTEGER,
     --INT to hold reference to employee role--
-    FOREIGN KEY role_id REFERENCES role(id)    
+    FOREIGN KEY role_id REFERENCES role(id),
+     --INT to hold reference to another employee that is manager of the current employee--   
+    FOREIGN KEY manager_id REFERENCES employee(id)
 )
 
 
