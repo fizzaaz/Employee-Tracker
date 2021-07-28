1793e42a3e5d36be0508670eef50a8f6ff464abc
unknown <fizz.zehra14@gmail.com>
2021-07-28 07:24:46 -0500

    package json installed

---
+++ server.js
@@ -0,0 +1,8 @@
+const connection = require("config/connectin.js")
+
+//========== Connection ID ==========================//
+connection.connect(function(err) {
+    if (err) throw err
+    console.log("Connected as Id" + connection.threadId)
+    startPrompt();
+});
